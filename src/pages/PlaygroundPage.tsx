8bd4786fd5be08b884e97f55e0d881ee6541f74e
A.Aldulaylan <A.Aldulaylan@TamkeenTech.SA>
2024-08-11 12:01:35 +0300

    Create a basic React project

---
+++ src/pages/PlaygroundPage.tsx
@@ -0,0 +1,8 @@
+import Onboarding from "../components/Onboarding";
+import TermsAndConditions from "../components/TermsAndConditions";
+
+const PlaygroundPage = () => {
+  return <TermsAndConditions />;
+};
+
+export default PlaygroundPage;
